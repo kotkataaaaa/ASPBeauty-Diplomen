6c734da1c2ec64fb5991fb00472720dd98f91f53
MihaelaK <mihaelakostova358@gmail.com>
2026-05-18 17:15:12 +0300

    promotions

--- ASPBeauty/obj/Debug/net8.0/ASPBeauty.AssemblyInfo.cs
+++ ASPBeauty/obj/Debug/net8.0/ASPBeauty.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPBeauty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3cb042cf77934bbe8c47bfd05b92ebf61bdce7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa3ffe5fe8c9ed1cfecc343af3063b4e8140c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPBeauty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPBeauty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
